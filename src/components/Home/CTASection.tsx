376939565badf72e57a9206ab0d48d7e76418b17
Varun Yadav <yadav.varun056038@gmail.com>
2025-04-06 19:26:23 +0530

    Clerk Authentication Done

--- src/components/Home/CTASection.tsx
+++ src/components/Home/CTASection.tsx
@@ -24,7 +24,7 @@ const CTASection = () => {
                 className="w-full min-[400px]:w-auto bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 text-white transition-all duration-300"
               >
                 <Link
-                  href="/pricing"
+                  href="/#pricing"
                   className="flex items-center justify-center"
                 >
                   Get Started{" "}
